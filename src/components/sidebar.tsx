83491c888514cc197faceece398d4d95a3274a19
Aria Nur Jamal <jamal.psw@gmail.com>
2022-08-30 08:07:59 +0700

    finishing sideboard

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,15 +1,13 @@
-import React from "react";
-// import DashboardIcon from "@mui/icons-material/Dashboard";
-// import AssignmentTurnedInIcon from "@mui/icons-material/AssignmentTurnedIn";
-// import PersonIcon from "@mui/icons-material/Person";
-// import InventoryIcon from "@mui/icons-material/Inventory";
-// import EqualizerIcon from "@mui/icons-material/Equalizer";
-// import LogoutIcon from "@mui/icons-material/Logout";
+import { useState } from "react";
+import LogoutIcon from "@mui/icons-material/Logout";
 
 import style from "./sidebar.module.css";
 import { SidebarData } from "../assets/data";
 
 export default function Sidebar() {
+  const [selected, setSelected] = useState(0)
+
+
   return (
     <div className={style.Sidebar}>
       <div className={style.Logo}>
@@ -18,12 +16,18 @@ export default function Sidebar() {
       </div>
 
       <div className={style.Menu}>
-        {SidebarData.map((item) => (
-          <div className={style.MenuItem}>
+        {SidebarData.map((item, index) => (
+          <div key={index} className={selected === index ? `${style.MenuItem} ${style.active}` : style.MenuItem}
+            onClick={() => setSelected(index)}
+          >
             <item.icon />
             <span>{item.heading}</span>
           </div>
         ))}
+        <div className={style.MenuItem}>
+          <LogoutIcon />
+          <span>Logout</span>
+        </div>
       </div>
     </div>
   );
